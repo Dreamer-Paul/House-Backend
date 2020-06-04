95a7cac1b2cc34d338173f78485e32ac65f465f0
Innei <tukon479@gmail.com>
2020-06-04 10:39:50 +0800

    style: format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import "./style/index.css";
-import App from "./components/app.tsx";
+/*
+ * @Author: Innei
+ * @Date: 2020-05-20 17:49:04
+ * @LastEditTime: 2020-06-04 10:38:31
+ * @LastEditors: Innei
+ * @FilePath: /House-Backend/src/index.js
+ * @Coding with Love
+ */
 
-export default App;
+import './style/index.css'
+import App from './components/app.tsx'
+import 'kico-style'
+
+export default App
